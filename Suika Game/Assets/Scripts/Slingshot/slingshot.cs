97a393a333c1210fabcbef97ad4f46c3f854ee24
Ewa <emiklewska@gmail.com>
2024-06-10 17:39:18 +0200

    proca

---
+++ Suika Game/Assets/Scripts/Slingshot/slingshot.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class slingshot : MonoBehaviour
+{
+    public LineRenderer[] lineRenderers;
+    public Transform[] stripPositions;
+    public Transform center;
+    public Transform idlePosition;
+
+    bool isMouseDown;
+
+    void Start()
+    {
+        lineRenderers[0].positionCount = 2;
+        lineRenderers[1].positionCount = 2;
+        lineRenderers[0].SetPosition(0, stripPositions[0].position);
+        lineRenderers[1].SetPosition(0, stripPositions[1].position);
+    }
+
+    void Update()
+    {
+        if (isMouseDown)
+        {
+            Vector3 mousePosition = Input.mousePosition;
+            mousePosition.z = 10;
+
+            mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+
+            SetStrips(mousePosition);
+        }
+        else
+        {
+            ResetStrips();
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        isMouseDown = true;
+    }
+
+    private void OnMouseUp()
+    {
+        isMouseDown = false;
+    }
+
+
+    void ResetStrips()
+    {
+        SetStrips(idlePosition.position);
+    }
+
+    void SetStrips(Vector3 position)
+    {
+        lineRenderers[0].SetPosition(1, position);
+        lineRenderers[1].SetPosition(1, position);
+    }
+}
